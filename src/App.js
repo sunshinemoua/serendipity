449629fd36cfa41ad02e414ad02a5fa9d251ab89
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2023-04-21 15:07:56 -0700

    first commit--created form with schema validation using react hook form and yup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink, BrowserRouter, Route } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import "./App.scss";
+
+const schema = yup.object().shape({
+  firstName: yup.string().required("First Name is required"),
+  lastName: yup.string().required("Last name is required"),
+  email: yup.string().email().required("Please enter a valid email address"),
+});
+
+const NavBar = () => {};
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    alert("thanks for signing up!");
+  };
+
+  return (
+    <div className="form-wrapper">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          <h1> Sign Up </h1>
+          <input {...register("firstName")} placeholder="First Name" />
+          <p>{errors.firstName?.message}</p>
+          <input {...register("lastName")} placeholder="Last Name" />
+          <p>{errors.lastName?.message}</p>
+          <input {...register("email")} placeholder="Email" />
+          <p>{errors.email?.message}</p>
+          <button>Submit</button>
+        </label>
+      </form>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1> serendipity </h1>
+      <Form />
+    </BrowserRouter>
   );
 }
 
